3c8ecf6707a1001fa7c53d16f6770ad3eec01ee4
John Montanye <gatewayitconsulting@outlook.com>
2020-05-12 23:09:11 -0500

    Implemented basic observable timer. Fixed time option styling.

--- tomato-timer-app/src/app/components/timer/timer.component.ts
+++ tomato-timer-app/src/app/components/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -9,7 +10,40 @@ export class TimerComponent implements OnInit {
 
   constructor() { }
 
+  timeLeft = 60;
+  tomatoTotal = 60;
+  interval;
+  subscribeTimer: any;
+
   ngOnInit() {
+  }
+
+  oberserableTimer() {
+    const source = timer(1000, 2000);
+    const abc = source.subscribe(val => {
+      console.log(val, '-');
+      this.subscribeTimer = this.timeLeft - val;
+    });
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = this.tomatoTotal;
+      }
+    }, 1000);
+  }
+
+  pause() {
+    clearInterval(this.interval);
+  }
+
+  restart() {
+    this.interval = setInterval(() => {
+      this.timeLeft = this.tomatoTotal;
+    });
   }
 
 }
